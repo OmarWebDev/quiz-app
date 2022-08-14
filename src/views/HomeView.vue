e27701ca9f377af3beacfdda5ea7fde9227eb488
Omar Khaled <omar.dev.work@outlook.com>
2022-08-14 22:58:01 +0200

    Fixed a typo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import { RouterLink } from "vue-router";
     <div class="container text-center">
       <h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100 mb-4">Quiz App!</h1>
       <h3 class="text-xl text-gray-700 dark:text-gray-300 mb-4">
-        Let's test your knowledge about Computer Scince
+        Let's test your knowledge about Computer Science
       </h3>
       <RouterLink to="/quiz">
         <Button>
